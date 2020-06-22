ab0af018a713efbaf077765e417a1896d8cd16bc
MarcosLudgerio <raimundo.marcos@dce.ufpb.br>
2020-06-22 10:14:38 -0300

    created project

---
+++ First-Lab/src/main/java/br/ufpb/dcx/cursospring/First/Lab/FirstLabApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.cursospring.First.Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstLabApplication.class, args);
+	}
+
+}
---
+++ First-Lab/src/test/java/br/ufpb/dcx/cursospring/First/Lab/FirstLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.cursospring.First.Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
